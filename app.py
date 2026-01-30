bd50cf59b7fe3fc416078fa3c94ff23b892e4f9f
Anant Jejani <147914369+Anant077@users.noreply.github.com>
2026-01-30 17:32:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
 import streamlit as st
-import yfinance as yf
-import pandas as pd
-import matplotlib.pyplot as plt
+from strategy import run_backtest
 
-st.set_page_config(layout="wide")
 st.title("Gold Trading Strategy Backtest")
 
-# User Inputs
-col1, col2 = st.columns(2)
-with col1:
-    start_date = st.date_input("Start Date")
-with col2:
-    end_date = st.date_input("End Date")
 
+start_date = st.date_input("Start Date")
+end_date = st.date_input("End Date")
+
+# BUTTON
 if st.button("Run Backtest"):
-    gold = yf.download("GC=F", start=start_date, end=end_date)
+    df = run_backtest(start_date, end_date)
 
-    gold["Return"] = gold["Close"].pct_change()
-    gold["Equity"] = (1 + gold["Return"]).cumprod()
+    st.subheader("Equity Curve")
+    st.line_chart(df["Equity"])
 
-    fig, ax = plt.subplots()
-    ax.plot(gold.index, gold["Equity"])
-    ax.set_title("Equity Curve")
-
-    st.pyplot(fig)
-    st.write(gold.tail())
+    st.subheader("Last Rows")
+    st.dataframe(df.tail())
